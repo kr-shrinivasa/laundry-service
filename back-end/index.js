206f7035ac8bc10cfc626860031f47037fa8f894
DyapaHarishwarReddy <harishwar72@gmail.com>
2022-01-24 13:50:54 +0530

    usermodels

--- back-end/index.js
+++ back-end/index.js
@@ -1,2 +1,30 @@
 
+const express = require('express')
+const app =express()
+
+
+const mongoose = require('mongoose')
+//connecting to DB
+mongoose.connect('mongodb://localhost:27017/laundry-service',()=>{
+    console.log("mongosee connected");
+});
+
+//external middleware
+const cors = require('cors')
+app.use(cors())
+
+//built-in middleware
+app.use(express.json())
+
+
+//custom middleware
+app.use( require("./routes/auth"))
+
+
+const PORT= 5000
+
+//creating server
+app.listen(PORT,()=>{
+    console.log("app is listing at port 5000");
+})
 
