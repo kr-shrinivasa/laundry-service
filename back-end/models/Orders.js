aa1b9a7a712188bc77bb460718c2e08b6ef1d0af
jigyasha1234 <jigrani0000@gmail.com>
2022-01-24 17:08:03 +0530

    some change in component

--- back-end/models/Orders.js
+++ back-end/models/Orders.js
@@ -1,2 +1,63 @@
 
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ordersSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+  
+    shippingInfo: {
+        address: {
+          type: String,
+          required: true,
+        },
+        storeAddress: {
+          type: String,
+          required: true,
+        },
+        phoneNo: {
+          type: Number,
+          required: true,
+        },
+      },
+
+      orderItems:[
+        {
+          name: {
+            type: String,
+            required: true,
+          },
+          price: {
+            type: Number,
+            required: true,
+          },
+          quantity: {
+            type: Number,
+            required: true,
+          },  
+        
+        washtypes:[
+            {
+                name: {
+                    type: String,
+                    required: true,
+                  },
+                washprice:{
+                    type: Number,
+                    required: true,
+                }
+            }
+        ]
+    }
+      ],
+      createdAt: {
+        type: Date,
+        default: Date.now,}
+      
+    });
+    
+    module.exports = mongoose.model("Order", ordersSchema);
+
 
---
+++ front-end/src/component/Login.js
@@ -0,0 +1,73 @@
+
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom'
+
+
+export default function Login() {
+
+    const [newobject, setnewobject] = useState({emailPhone:"",password:""})
+
+    // console.log(newobject)
+
+    function handlechange(e){
+        // console.log(e.target.value);
+        // console.log(e.target.name);
+            setnewobject({...newobject,[e.target.name]:e.target.value})
+            // console.log(newobject)
+    }
+    function handlesubmit(e){
+        e.preventDefault()
+        console.log(newobject)
+    
+       if( newobject.emailPhone===Number){
+            
+       }
+    }
+
+
+
+  return (
+      <section className='login-page'>
+
+
+    <div className='left-login'>
+
+        <div className='content'>
+        <h1 className='title-login'>Laundry Service</h1>
+        <p className='para-login'>Doorstep Wash & Dryclean service</p>
+        <div className='btn-sec'>
+            <p>Don't Have An Account?</p>
+            <NavLink className="li" to={"/register"} ><button className='reg-btn'>Register</button></NavLink>
+        </div>
+
+        </div>
+    </div>
+
+
+  <div className='right-login'>
+      <div className='form-login'>
+      <h3>SIGN IN</h3>
+      <form action="" onSubmit={handlesubmit}>
+        <div className='field'>
+            <label htmlFor="Email/phone">Mobile/Email</label>
+            <input type="text" id='Email/phone'onChange={handlechange} name="emailPhone"/>
+        <div className='under-line'></div>
+        </div>
+        <div className='field'>
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password'onChange={handlechange} name="password"/>
+            <div className='under-line'></div>
+            <div className='forgot'>
+                <p>Forgot Password?</p>
+            </div>
+        </div>
+
+        <button className='btn-signin'>Sign In</button>
+        
+      </form >
+      </div>
+
+  </div>
+  </section>
+  )
+}
